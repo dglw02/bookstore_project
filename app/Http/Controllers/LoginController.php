1b6f5b36c9315004cc8919ba6e39fe576898e15b
dglw02 <luuhaiduong100@gmail.com>
2022-12-09 10:35:32 +0700

    aaa
    aa

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -37,5 +37,6 @@ class LoginController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('/');
+        //aaaaaa
     }
 }
