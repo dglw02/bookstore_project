cd374f2994555fe185a5c53bada3d8681c3713b9
Trần Minh Đạt <lopperfuture@gmail.com>
2022-12-19 09:14:36 +0700

    cart thanh cong, orders next

---
+++ app/Http/Controllers/SortBooksController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Books;
+use App\Models\Category;
+use Request;
+
+class SortBooksController extends Controller
+{
+    function allBooks(){
+        $data = Books::all();
+        if(Request::get('sort')=='price_asc'){
+            $data = Books::orderBy('books_price','ASC')->get();
+        }
+        elseif(Request::get('sort') =='price_desc'){
+            $data = Books::orderBy('books_price','DESC')->get();
+        }
+        return view('allbooks',['books'=>$data]);
+    }
+
+    function productsByCategory($category_name){
+        $category =Category::where('category_name',$category_name)->first();
+        $books = $category->books()->get();
+        $category_id = $category->category_id;
+        if(Request::get('sort')=='price_asc'){
+            $books = $category->books()->where('category_id',$category_id)->orderBy('books_price','ASC')->get();
+        }
+        elseif(Request::get('sort') =='price_desc'){
+            $books = $category->books()->where('category_id',$category_id)->orderBy('books_price','DESC')->get();
+        }
+        return view('category.index',compact('category','books',));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BooksController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Admin\AdminBooksController;
+use App\Http\Controllers\SortBooksController;
 use App\Http\Controllers\Admin\AdminCategoryController;
 use App\Http\Controllers\Admin\AdminAuthorController;
 use App\Http\Controllers\Admin\AdminOrdersController;
@@ -97,7 +98,7 @@ Route::post('/logout',[LoginController::class,'logout']);
 
 //Books
 Route::get('/',[BooksController::class,'showBooks']);
-Route::get('/allbooks',[BooksController::class,'allBooks']);
+Route::get('/allbooks',[SortBooksController::class,'allBooks']);
 Route::get('/detail/{id}',[BooksController::class,'showDetail']);
 Route::post('/cart/{id}',[BooksController::class,'addCart']);
 Route::get('search',[BooksController::class,'search']);
@@ -105,4 +106,4 @@ Route::get('new',[BooksController::class,'newestBook']);
 
 //Category
 Route::get('/category',[BooksController::class,'allCategory']);
-Route::get('/category/{category_name}',[BooksController::class,'productsByCategory']);
+Route::get('/category/{category_name}',[SortBooksController::class,'productsByCategory']);
