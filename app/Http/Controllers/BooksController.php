a090c8dce440b4f32d97482eb76faa77ee4f055e
dglw02 <luuhaiduong100@gmail.com>
2022-12-19 14:30:53 +0700

    ....

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\Books;
@@ -16,7 +16,7 @@ class BooksController extends Controller
     //
     function showBooks(){
         $books = Books::all();
-        $author = Author::all();
+        $author =Author::all();
         $category =Category::all();
         $newbook =Books::orderBy('created_at','DESC')->get()->take(10);
         return view('home',compact('books','author','category','newbook'));
@@ -27,6 +27,17 @@ class BooksController extends Controller
         return view('category',compact('categories'));
     }
 
+
+    function allBooks(){
+        $data = Books::all();
+        if(Request::get('sort')=='price_asc'){
+            $data = Books::orderBy('books_price','ASC')->get();
+        }
+        elseif(Request::get('sort') =='price_desc'){
+            $data = Books::orderBy('books_price','DESC')->get();
+        }
+        return view('allbooks',['books'=>$data]);
+    }
 
 
     function showDetail($books_id){
@@ -41,6 +52,19 @@ class BooksController extends Controller
     }
 
 
+    function productsByCategory($category_name){
+        $category =Category::where('category_name',$category_name)->first();
+        $books = $category->books()->get();
+        $category_id = $category->category_id;
+        if(Request::get('sort')=='price_asc'){
+            $books = $category->books()->where('category_id',$category_id)->orderBy('books_price','ASC')->get();
+        }
+        elseif(Request::get('sort') =='price_desc'){
+        $books = $category->books()->where('category_id',$category_id)->orderBy('books_price','DESC')->get();
+        }
+        return view('category.index',compact('category','books',));
+    }
+
 //new page
     function newestBook(){
         $books =Books::orderBy('created_at','DESC')->get()->take(10);
@@ -50,6 +74,7 @@ class BooksController extends Controller
     function addCart(Request $request ,$books_id){
         if(Auth::id())
         {
+            ///
             $user =auth()->user();
             $books=Books::find($books_id);
             $cart =new cart;
