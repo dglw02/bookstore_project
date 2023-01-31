388a2c46ad32de0c6b8b807057d2143924aa5fcb
Trần Minh Đạt <lopperfuture@gmail.com>
2023-01-31 21:24:37 +0700

    .

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -120,7 +120,7 @@
             <div class="col-xl-4 col-lg-5">
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Pending orders</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Latest orders</h6>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
