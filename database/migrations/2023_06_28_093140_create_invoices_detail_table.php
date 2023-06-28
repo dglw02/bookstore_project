d88e6a62cf254f28c3121f6c22bea185ec0a8429
dglw02 <luuhaiduong100@gmail.com>
2023-06-28 16:39:45 +0700

    update 2 bảng invoices/invoicesDetail(bảng Nhập)

---
+++ database/migrations/2023_06_28_093140_create_invoices_detail_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices_detail', function (Blueprint $table) {
+            $table->increments('invoices_detail_id');
+            $table->unsignedInteger('invoices_id');
+            $table->unsignedInteger('books_id');
+            $table->timestamps();
+            $table->foreign('invoices_id')->references('invoices_id')->on('invoices');
+            $table->foreign('books_id')->references('books_id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices_detail');
+    }
+};
