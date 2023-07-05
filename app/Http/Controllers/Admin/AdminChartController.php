b6642f8671c6d95595c1fb4d6d7abef1c9de3f3f
Trần Minh Đạt <lopperfuture@gmail.com>
2023-07-05 16:03:32 +0700

    tạm thời xong thống kê, cần cải thiện thêm

--- app/Http/Controllers/Admin/AdminChartController.php
+++ app/Http/Controllers/Admin/AdminChartController.php
@@ -10,12 +10,16 @@ use Illuminate\Support\Facades\DB;
 class AdminChartController extends Controller
 {
     public function index(){
-        $orders = Order::where('orders_status', '3')->select(DB::raw("sum(orders_price) as orders_price"))
+        $orders = Order::where('orders_status', '3')->select(DB::raw("sum(orders_price) as orders_price"), DB::raw("MONTHNAME(created_at) as month_name"))
             ->whereYear('created_at', date('Y'))
-            ->groupBy(DB::raw("Month(created_at)"))
-            ->pluck('orders_price');
+            ->groupBy(DB::raw("month_name"))
+            ->orderBy('orders_id','ASC')
+            ->pluck('orders_price', 'month_name');
 
-        return view('admin.chart.month_revenue', compact('orders'));
+        $labels = $orders->keys();
+        $data = $orders->values();
+
+        return view('admin.chart.month_revenue', compact('labels', 'data'));
 
     }
 }
