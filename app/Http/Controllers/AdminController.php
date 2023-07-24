ebb9848a3a192f366b476e002db2b1f09acf7953
dglw02 <luuhaiduong100@gmail.com>
2023-07-24 15:12:11 +0700

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\Invoice;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Books;
@@ -21,42 +22,49 @@ class AdminController extends Controller
 
     function viewDashboard()
     {
-        $users = User::where('isAdmin','false');
-        $total_earning = Order::where('orders_status', '3')->sum('orders_price');
-        $total_import = Invoice::all()->sum('invoices_total');
-        $completed_orders = Order::where('orders_status', '3')->get();
-        $pending_orders = Order::where('orders_status', '0')->get();
+            $user = Auth::user();
+            if (is_null($user)){
+                return redirect('/');
+            }
+            else{
+                $users = User::where('isAdmin','false');
+                $total_earning = Order::where('orders_status', '3')->sum('orders_price');
+                $total_import = Invoice::all()->sum('invoices_total');
+                $completed_orders = Order::where('orders_status', '3')->get();
+                $pending_orders = Order::where('orders_status', '0')->get();
 
 
-        $earning_month = Order::where('orders_status', '3')
-            ->whereMonth('created_at', Carbon::now()->month)
-            ->sum('orders_price');
-        $import_month = Invoice::select('*')
-            ->whereMonth('invoices_date', Carbon::now()->month)
-            ->sum('invoices_total');
+                $earning_month = Order::where('orders_status', '3')
+                    ->whereMonth('created_at', Carbon::now()->month)
+                    ->sum('orders_price');
+                $import_month = Invoice::select('*')
+                    ->whereMonth('invoices_date', Carbon::now()->month)
+                    ->sum('invoices_total');
 
 
-        $earning_year = Order::where('orders_status', '3')
-            ->whereYear('created_at', Carbon::now()->year)
-            ->sum('orders_price');
-        $import_year = Invoice::select('*')
-            ->whereYear('invoices_date', Carbon::now()->year)
-            ->sum('invoices_total');
+                $earning_year = Order::where('orders_status', '3')
+                    ->whereYear('created_at', Carbon::now()->year)
+                    ->sum('orders_price');
+                $import_year = Invoice::select('*')
+                    ->whereYear('invoices_date', Carbon::now()->year)
+                    ->sum('invoices_total');
 
-        $top = OrderDetails::with('books')
-            ->join('orders', 'orders.orders_id', '=', 'order_details.orders_id')
-            ->select('books_id', DB::raw('SUM(quantity) as count'))
-            ->where('orders.orders_status', '3')
-            ->groupBy('books_id')
-            ->orderBy("count", 'desc')
-            ->take(3)
-            ->get();
+                $top = OrderDetails::with('books')
+                    ->join('orders', 'orders.orders_id', '=', 'order_details.orders_id')
+                    ->select('books_id', DB::raw('SUM(quantity) as count'))
+                    ->where('orders.orders_status', '3')
+                    ->groupBy('books_id')
+                    ->orderBy("count", 'desc')
+                    ->take(3)
+                    ->get();
 
-        $orders = Order::where('orders_status', '0')->get();
+                $orders = Order::where('orders_status', '0')->get();
 
 
-        return view('admin/dashboard', compact('users', 'completed_orders','total_import','earning_month','import_month','earning_year','import_year',
-            'total_earning', 'pending_orders', 'top', 'orders'));
+                return view('admin/dashboard', compact('users', 'completed_orders','total_import','earning_month','import_month','earning_year','import_year',
+                    'total_earning', 'pending_orders', 'top', 'orders'));
+
+            }
     }
 
     function viewAllCategory()
